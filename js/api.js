804bb4d167e07a2518710f55dac80f88c2f11bc0
DavidIgnatiev <ignatiev.david@gmail.com>
2026-03-18 15:53:49 +0000

    Fix paths and https

--- js/api.js
+++ js/api.js
@@ -1,13 +1,13 @@
 const API_KEY = "24283a68";
 
 export async function getMovie(movie) {
-    const firstPage = await fetch(`http://www.omdbapi.com/?s=${movie}&apikey=${API_KEY}`).then(r => r.json());
+    const firstPage = await fetch(`https://www.omdbapi.com/?s=${movie}&apikey=${API_KEY}`).then(r => r.json());
     if (firstPage.Response === "False") return [];
     const totalResults = firstPage.totalResults;
     const numberOfPages = Math.ceil(totalResults / 10);
     const promises = [];
     for (let i = 2; i <= numberOfPages; i++) {
-        promises.push(fetch(`http://www.omdbapi.com/?s=${movie}&apikey=${API_KEY}&page=${i}`).then(r => r.json()));
+        promises.push(fetch(`https://www.omdbapi.com/?s=${movie}&apikey=${API_KEY}&page=${i}`).then(r => r.json()));
     }
     const results = await Promise.all(promises);
     const allMovies = [firstPage, ...results].flatMap(r => r.Search || [])
@@ -17,6 +17,6 @@ export async function getMovie(movie) {
 
 
 export async function getMovieById(id) {
-    let result = await fetch(`http://www.omdbapi.com/?i=${id}&apikey=${API_KEY}`);
+    let result = await fetch(`https://www.omdbapi.com/?i=${id}&apikey=${API_KEY}`);
     return result.json();
 }
